f5e58afa7765f317eafa722f4e4dac939e7bf55b
sususuin <kokundntkipwr55@gmail.com>
2022-01-22 15:12:42 +0900

    新規追加

---
+++ player_input.h
@@ -0,0 +1,10 @@
+/*****************************************************************************/
+/* File Name : player_input.h                                                       */
+/*****************************************************************************/
+/* 多重インクルードガード */
+#ifndef PLAYER_INPUT_H
+#define PLAYER_INPUT_H
+
+extern BOOL player_input(int player,int num_table[3][3]);
+
+#endif  /* PLAYER_INPUT_H */
